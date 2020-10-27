9121e0852cce9c63623ff6c51d51edeef81b9ab9
keveniskander <keven.iskander@hotmail.com>
2020-10-26 20:25:08 -0400

    LoginActivity added

--- app/src/main/java/com/example/netflixmatchmaker/SplashActivity.java
+++ app/src/main/java/com/example/netflixmatchmaker/SplashActivity.java
@@ -2,13 +2,30 @@ package com.example.netflixmatchmaker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import com.example.netflixmatchmaker.ui.login.LoginActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static int SPLASH_TIME_OUT = 3000; //Loading time
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
