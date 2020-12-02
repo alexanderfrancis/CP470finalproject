9a90374ac0592d90d28d5e7c916dfc4bd98cb7c0
alexanderfrancis <ftbafrancis@gmail.com>
2020-12-01 19:48:57 -0500

    itemmodel changes

--- app/src/main/java/com/example/netflixmatchmaker/ui/explore_movies/Explore_Movies_Fragment.java
+++ app/src/main/java/com/example/netflixmatchmaker/ui/explore_movies/Explore_Movies_Fragment.java
@@ -125,16 +125,17 @@ public class Explore_Movies_Fragment extends Fragment {
 
     private List<ItemModel> addList() {
         List<ItemModel> items = new ArrayList<>();
-        items.add(new ItemModel(R.drawable.sample1, "Interstellar", "24", "Jember"));
-        items.add(new ItemModel(R.drawable.sample2, "Inception", "20", "Malang"));
-        items.add(new ItemModel(R.drawable.sample3, "Dunkirk", "27", "Jonggol"));
-        items.add(new ItemModel(R.drawable.sample4, "Tenet", "19", "Bandung"));
+        //items.add(new ItemModel(R.drawable.sample1, "Interstellar", "24", "Jember"));
+        items.add(new ItemModel("https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+                "Inception", "2010", "9.5"));
+        //items.add(new ItemModel(R.drawable.sample3, "Dunkirk", "27", "Jonggol"));
+        //items.add(new ItemModel(R.drawable.sample4, "Tenet", "19", "Bandung"));
 //        items.add(new ItemModel(R.drawable.sample5, "Marmut", "25", "Hutan"));
 
-        items.add(new ItemModel(R.drawable.sample1, "Markonah", "24", "Jember"));
-        items.add(new ItemModel(R.drawable.sample2, "Marpuah", "20", "Malang"));
-        items.add(new ItemModel(R.drawable.sample3, "Sukijah", "27", "Jonggol"));
-        items.add(new ItemModel(R.drawable.sample4, "Markobar", "19", "Bandung"));
+        //items.add(new ItemModel(R.drawable.sample1, "Markonah", "24", "Jember"));
+        //items.add(new ItemModel(R.drawable.sample2, "Marpuah", "20", "Malang"));
+        //items.add(new ItemModel(R.drawable.sample3, "Sukijah", "27", "Jonggol"));
+        //items.add(new ItemModel(R.drawable.sample4, "Markobar", "19", "Bandung"));
 //        items.add(new ItemModel(R.drawable.sample5, "Marmut", "25", "Hutan"));
         return items;
     }
