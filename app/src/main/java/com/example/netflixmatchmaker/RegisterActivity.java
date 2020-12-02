0229aedb8f479f3b10ee2294aa6994b1cc30193c
keveniskander <keven.iskander@hotmail.com>
2020-12-01 21:14:23 -0500

    Registration added (email)

--- app/src/main/java/com/example/netflixmatchmaker/RegisterActivity.java
+++ app/src/main/java/com/example/netflixmatchmaker/RegisterActivity.java
@@ -1,13 +1,79 @@
 package com.example.netflixmatchmaker;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.netflixmatchmaker.ui.login.LoginActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText mEmail, mPassword, mPasswordConfirmed;
+    private Button registerButton;
+
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+        mEmail = (EditText) findViewById(R.id.emailRegister);
+        mPassword = (EditText) findViewById(R.id.password);
+        mPasswordConfirmed = (EditText) findViewById(R.id.password2);
+        registerButton = (Button) findViewById(R.id.createAcc);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerNewUser();
+            }
+        });
+    }
+    private void registerNewUser() {
+
+        String email, password, password2;
+        email = mEmail.getText().toString();
+        password = mPassword.getText().toString();
+        password2 = mPasswordConfirmed.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Please enter email...", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Please enter password!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (!password.equals(password2)){
+            Toast.makeText(getApplicationContext(), "Password does not match Confirmed Password!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Registration successful!", Toast.LENGTH_LONG).show();
+
+                            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "Registration failed! Please try again later", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 }
